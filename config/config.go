2267dbc8052f035fc56b00f5631da955c69c62cb
getwe <honggengwei@gmail.com>
2014-04-01 15:35:43 +0800

    add goose/config

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+    "path/filepath"
+    "fmt"
+)
+
+type Conf interface {
+    String(key string) string
+    Int64(key string) int64
+    Float64(key string) float64
+}
+
+type confParser interface{
+    Parse(file string) (Conf,error)
+}
+
+func NewConf(file string) (Conf,error) {
+    suffix := filepath.Ext(file)
+    p,ok := parsers[suffix]
+    if !ok {
+        return nil,fmt.Errorf("can't parse file [%s]",file)
+    }
+    return p.Parse(file)
+}
+
+var parsers = make(map[string]confParser)
+
+func register(name string, parser confParser) {
+    _,ok := parsers[name]
+    if ok {
+        panic("duplicate parser")
+    }
+    parsers[name] = parser
+}
+
---
+++ config/toml.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+    "github.com/laurent22/toml-go"
+    "fmt"
+)
+
+type TomlConf struct {
+    conf            toml.Document
+}
+
+func (this *TomlConf) String(key string) string {
+    return this.conf.GetString(key)
+}
+
+func (this *TomlConf) Int64(key string) int64 {
+    return this.conf.GetInt64(key)
+}
+
+func (this *TomlConf) Float64(key string) float64 {
+    return this.conf.GetFloat64(key)
+}
+
+type tomlConfParser struct {
+}
+
+func (this *tomlConfParser) Parse(file string) (c Conf,err error) {
+    defer func() {
+        if r := recover();r != nil {
+            err = fmt.Errorf("%s",r)
+        }
+    }()
+
+    tconf := &TomlConf{}
+    var parser toml.Parser
+    tconf.conf = parser.ParseFile(file)
+
+    return tconf,nil
+}
+
+func init() {
+    register(".toml",&tomlConfParser{})
+}
