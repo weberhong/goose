ece06696ddcb18e45024f173a97d7b1bbd593518
getwe <honggengwei@gmail.com>
2014-04-07 00:08:49 +0800

    add log module

---
+++ log/log.go
@@ -0,0 +1,75 @@
+// log的简单封装
+// 我所期待的log接口(我在工作环境熟悉的)是这样的:
+// 1. 日志分为多个文件,一个普通INFO日志,一个DEBUG,另外一个是WARN,ERROR,FATAL类型
+// 2. INFO一般不采用调用一次打一行日志
+// 3. INFO一般一次逻辑处理只打一行
+// 4. 除了INFO之外,其它每调用一次,输出一行日志.
+package log
+
+import (
+    log4go "github.com/alecthomas/log4go"
+    config "github.com/getwe/goose/config"
+)
+
+var (
+    debugLogger log4go.Logger
+    infoLogger  log4go.Logger
+    errorLogger log4go.Logger
+)
+
+func init() {
+    debugLogger = log4go.NewDefaultLogger(log4go.DEBUG)
+    infoLogger  = log4go.NewDefaultLogger(log4go.INFO)
+    errorLogger = log4go.NewDefaultLogger(log4go.WARNING)
+}
+
+
+func newFileFilter(file string) (*log4go.FileLogWriter) {
+	flw := log4go.NewFileLogWriter(file, false)
+	flw.SetFormat("[%D %T] [%L] (%S) %M")
+	flw.SetRotateLines(0)
+	flw.SetRotateSize(0)
+	flw.SetRotateDaily(false)
+
+    return flw
+}
+
+func LoadConfiguration(confPath string) (error) {
+
+    conf,err := config.NewConf(confPath)
+    if err != nil {
+        return err
+    }
+
+    var filt *log4go.FileLogWriter
+
+    // debug
+    debug_enable := conf.Bool("debug.Enable")
+    debug_file := conf.String("debug.FileName")
+    filt = nil
+    if debug_enable {
+        filt = newFileFilter(debug_file)
+    }
+    debugLogger["debug"] = &log4go.Filter{log4go.DEBUG, filt}
+
+    // info
+    info_enable := conf.Bool("info.Enable")
+    info_file := conf.String("info.FileName")
+    filt = nil
+    if info_enable {
+        filt = newFileFilter(info_file)
+    }
+    infoLogger["info"] = &log4go.Filter{log4go.INFO,filt}
+
+    // error
+    error_enable := conf.Bool("error.Enable")
+    error_file := conf.String("info.FileName")
+    filt = nil
+    if error_enable {
+        filt = newFileFilter(error_file)
+    }
+    errorLogger["error"] = &log4go.Filter{log4go.WARNING,filt}
+
+    return nil
+}
+
---
+++ log/wrapper.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+    "fmt"
+)
+
+type GooseLogger struct {
+    logstr []string
+}
+
+func NewGooseLogger() (*GooseLogger) {
+    return &GooseLogger{}
+}
+
+func Warn(arg0 interface{}, args ...interface{}) error {
+    return errorLogger.Warn(arg0,args)
+}
+// Warn日志直接输出
+func (GooseLogger) Warn(arg0 interface{}, args ...interface{}) error {
+    return Warn(arg0,args)
+}
+
+func Error(arg0 interface{}, args ...interface{}) error {
+    return errorLogger.Error(arg0,args)
+}
+// Error日志直接输出
+func (GooseLogger) Error(arg0 interface{}, args ...interface{}) error {
+    return Error(arg0,args)
+}
+
+func Debug(arg0 interface{}, args ...interface{}) error {
+    debugLogger.Debug(arg0,args)
+    return nil
+}
+// Debug日志直接输出
+func (GooseLogger) Debug(arg0 interface{}, args ...interface{}) error {
+    Debug(arg0,args)
+    return nil
+}
+
+// Info日志先存起来
+func (this *GooseLogger) Info(format string, args ...interface{}) error {
+    this.logstr = append(this.logstr,fmt.Sprintf(format,args...))
+    return nil
+}
+
+// 输出全部Info日志
+func (this *GooseLogger) PrintAllInfo() error {
+    infoLogger.Info(this.logstr)
+    this.logstr = this.logstr[:0]
+    return nil
+}
+
+
